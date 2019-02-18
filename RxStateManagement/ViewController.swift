d30bb9d87acd7a58b5a2fd690d6b6204b8a96ed7
Justin Steffen <justindsn@gmail.com>
2019-02-18 12:48:56 -0800

    Initial Commit

---
+++ RxStateManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxStateManagement
+//
+//  Created by Anna M Roberts on 2/18/19.
+//  Copyright © 2019 Justin D. Steffen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
